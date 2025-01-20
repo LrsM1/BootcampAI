c107ac76eb6951497eb3f7b6f2d8f167dbc80409
Martina La Rosa <martinalarosa0704@gmail.com>
2025-01-20 17:47:54 +0100

    Giorno 0 Bonus

---
+++ main.py
@@ -0,0 +1,10 @@
+# Esercizio 0
+print("Esercizio 0")
+print("Scrivere un programma in Python che stampi a schermo il seguente:")
+print('0. La scritta "ciao, mondo!"')
+print("1. Il risultato della somma dei numeri 40 e 2")
+print("2. Il risultato della divisione del numero 40 per il numero 0 ;)")
+## Bonus
+print("Bonus")
+print("Scrivere un altro programma in Python che stampi a schermo il seguente:")
+print("0. il contenuto di questo file `README.md`")
